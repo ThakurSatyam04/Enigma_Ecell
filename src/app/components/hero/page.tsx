d8a71d08bacec5913b5701246691e320aa8d6eef
SatyamKumar <kumarsatyam04.2000@gmail.com>
2023-09-16 09:50:47 +0530

    commit all

--- src/app/components/hero/page.tsx
+++ src/app/components/hero/page.tsx
@@ -7,32 +7,33 @@ import icon1 from '@/app/Icons/chevrons-down.svg'
 
 const Heropage = () => {
   return (
-    <div>
-        {/* Hero Logo */}
-      <div className='w-full flex items-center justify-center'>
-        <Image className='w-[80px] h-[80px]' src={logo} alt='Enigma Logo'/>
-      </div>
+    <div className=''>
+  {/* Hero Logo */}
+  <div className='w-full flex items-center justify-center'>
+    <Image className='w-[80px] h-[80px]' src={logo} alt='Enigma Logo' />
+  </div>
 
-      <div className='w-full flex items-center justify-center'>
-        <NavbarPage/>
-      </div>
+  {/* Navbar - Uncomment when you have the Navbar component */}
+  <div className='w-full flex items-center justify-center '>
+    <NavbarPage />
+  </div>
 
-      <div className='h-full flex items-center justify-evenly gap-20 glex-wrap'>
-        <div className='w-4/12'>
-            <Image src={logo2} alt='Enigma logo' />
-        </div>
-        <div className='flex flex-col items-center justify-center'>
-          <div className='min-h-[300px] border-r border-white text-white'></div>
-          <div><Image src={icon1} alt='Icon'/></div>
-        </div>
-        <div className='text-white text-4xl flex flex-col items-center justify-center gap-5 relative'>
-            <div>THE</div>
-            <div>ENTREPRENEURSHIP</div>
-            <div>CELL OF <span className='text-[#F26938]'>NMIT</span></div>
-            <div> </div>
-        </div>
-      </div>
+  <div className='h-full flex flex-col sm:flex-row items-center justify-evenly gap-0 sm:gap-20 glex-wrap'>
+    <div className='w-full md:w-4/12 flex items-center justify-center '>
+      <Image src={logo2} alt='Enigma logo' />
     </div>
+    <div className='hidden md:flex flex-col items-center justify-center'>
+      <div className='min-h-[300px] border-r border-white text-white'></div>
+      <div><Image src={icon1} alt='Icon'/></div>
+    </div>
+    <div className='text-white text-2xl sm:text-4xl flex flex-col items-center justify-center gap-5'>
+      <div>THE</div>
+      <div>ENTREPRENEURSHIP</div>
+      <div>CELL OF <span className='text-[#F26938]'>NMIT</span></div>
+    </div>
+  </div>
+</div>
+
   )
 }
 
--- src/app/components/navbar/page.tsx
+++ src/app/components/navbar/page.tsx
@@ -1,22 +1,84 @@
-import React from 'react'
+// components/Navbar.js
+'use client'
+import React, { useState } from 'react';
 
 const NavbarPage = () => {
-  return (
-    <div>
-      <div className='bg-[#242526] p-1 rounded-md flex items-center justify-around'>
-        <ul className='text-[#F26938] flex gap-10 sm:ml-10 sm:mr-10'>
-            <li className='navbar_li'>Home</li>
-            <li className='navbar_li'>About</li>
-            <li className='navbar_li'>Recruitment’ 23</li>
-            <li className='navbar_li'>Annual Report’ 21</li>
-            <li className='navbar_li'>Ideathon’ 23</li>
-            <li className='navbar_li'>Blogs</li>
-            <li className='navbar_li'>Team</li>
-            <li className='navbar_li'>Contact Us</li>
-        </ul>
-      </div>
-    </div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default NavbarPage
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className='bg-[#242526] p-4 rounded-md flex flex-col sm:flex-row items-center justify-between'>
+        <button
+          className='text-[#F26938] text-2xl sm:hidden focus:outline-none '
+          onClick={toggleMenu}
+        >
+          {!isOpen ? (
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              className='h-6 w-6'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M4 6h16M4 12h16M4 18h16'
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              className='h-6 w-6'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M6 18L18 6M6 6l12 12'
+              />
+            </svg>
+          )}
+        </button>
+      <ul
+        className={`${
+          isOpen ? 'block' : 'hidden'
+        } sm:flex mt-4 sm:mt-0 sm:flex-row gap-2 sm:gap-10 sm:ml-10 sm:mr-10`}
+      >
+        <li className='navbar_li'>
+          <a href="/">Home</a>
+        </li>
+        <li className='navbar_li'>
+          <a href="/about">About</a>
+        </li>
+        <li className='navbar_li'>
+          <a href="/recruitment">Recruitment 23</a>
+        </li>
+        <li className='navbar_li'>
+          <a href="/annual-report">Annual Report 21</a>
+        </li>
+        <li className='navbar_li'>
+          <a href="/ideathon">Ideathon 23</a>
+        </li>
+        <li className='navbar_li'>
+          <a href="/blogs">Blogs</a>
+        </li>
+        <li className='navbar_li'>
+          <a href="/team">Team</a>
+        </li>
+        <li className='navbar_li'>
+          <a href="/contact">Contact Us</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavbarPage;
